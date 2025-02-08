bbe45a78c66c9d169ed5eb09e3c674bc56465e22
deo <deoninja@gmail.com>
2025-02-08 16:15:29 +0800

    backend setup

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,6 +1,10 @@
 import express from "express";
+import authRoutes from "./routes/auth.route.js";
+// import authRoutes from "./routes/auth.route";
 
 const app = express();
+
+app.use("/api/auth", authRoutes);
 
 app.listen(5001, () => {
     console.log("Server is running on port 5001")

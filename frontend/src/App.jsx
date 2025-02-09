9dba564f1f50b2b3ac79cde48af316f8b9c86ac4
deo <deoninja@gmail.com>
2025-02-10 00:07:24 +0800

    signup and login page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,19 +1,50 @@
-import React from 'react'
-import "./App.css"
+import React, { useEffect } from 'react'
+import Navbar from './components/Navbar'
+import HomePage from './pages/HomePage'
+import SignUpPage from './pages/SignUpPage'
+import LoginPage from './pages/LoginPage'
+import SettingsPage from './pages/SettingsPage'
+import ProfilePage from './pages/ProfilePage'
+// import ChatPage from './pages/ChatPage'
+
+import {Routes, Route, Navigate} from 'react-router-dom'
+import { useAuthStore } from './store/useAuthStore'
+import { Loader } from 'lucide-react'
+import { Toaster } from 'react-hot-toast'
 
 const App = () => {
+  const {authUser, checkAuth, isCheckingAuth} = useAuthStore();
+
+  useEffect(() =>{
+    checkAuth();
+  }, [authUser]);
+
+  console.log({ authUser });
+
+  if(isCheckingAuth && !authUser){
+    return (
+      <div className='flex items-center justify-center h-screen'>
+      <Loader className='size-10 animate-spin'/>
+    </div>
+    );
+  }
+  
   return (
     <div>
-      <h1 className='text-2xl font-bold text-red-500'>Hello World!</h1>
-      <button className="btn">Button</button>
-<button className="btn btn-neutral">Neutral</button>
-<button className="btn btn-primary">Primary</button>
-<button className="btn btn-secondary">Secondary</button>
-<button className="btn btn-accent">Accent</button>
-<button className="btn btn-ghost">Ghost</button>
-<button className="btn btn-link">Link</button>
+      <Navbar />
+
+      <Routes>
+        <Route path="/" element={authUser ? <HomePage /> : <Navigate to="/login" />} />
+        <Route path="/signup" element={!authUser ? <SignUpPage /> : <Navigate to="/" />} />
+        <Route path="/login" element={!authUser ? <LoginPage /> : <Navigate to="/" />} />
+        <Route path="/setting" element={ <SettingsPage /> } />
+        <Route path="/profile" element={authUser ? <ProfilePage /> : <Navigate to="/login" />} />
+        {/* <Route path="/chat" element={<ChatPage />} /> */}
+      </Routes>
+
+      <Toaster />
     </div>
-  )
-}
+  );
+};
 
 export default App
---
+++ frontend/src/pages/SettingsPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const SettingsPage = () => {
+  return (
+    <div>
+      <h2>SettingsPage</h2>
+    </div>
+  )
+}
+
+export default SettingsPage
